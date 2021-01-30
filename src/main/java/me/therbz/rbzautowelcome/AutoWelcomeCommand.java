5978f6807c6e548608989ea5347f55bbb9a8fa7e
TheRBZ <benhansen108@gmail.com>
2021-01-30 21:19:50 +0000

    Bump version to 1.5.0

--- src/main/java/me/therbz/rbzautowelcome/AutoWelcomeCommand.java
+++ src/main/java/me/therbz/rbzautowelcome/AutoWelcomeCommand.java
@@ -1,6 +1,5 @@
 package me.therbz.rbzautowelcome;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
